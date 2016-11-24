90e30eb5ecb1f09914c8d85a795f16639aa9de14
Ariet <ariet.nyshanbaev@iaau.edu.kg>
2016-11-24 20:16:13 +0300

    have came from kindergarten and had a dinner

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^create_quote$', views.create_quote, name='create_quote'),
+    url(r'^edit_quote/(?P<quote_id>.+)$', views.edit_quote, name='edit_quote'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 ﻿from django.shortcuts import render
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+
 from .models import Quote
 
 def main(request):
@@ -12,6 +14,21 @@ def create_quote(request):
 	if request.POST:
 		quote_body = request.POST.get('quote_body', '')
 		Quote.objects.create(body=quote_body, user=request.user)
-		messages.info(request, 'You have successfully crated a quote.')
+		messages.info(request, 'You have successfully crated this quote.')
 	template = 'main/create_quote.html'
 	return render(request, template, {})
+
+def edit_quote(request, quote_id):
+	""" Edit quote view """
+	args = {}
+	quote = get_object_or_404(Quote, pk=quote_id, user=request.user)
+
+	if request.POST:
+		quote_body = request.POST.get('quote_body', '')
+		quote.body = quote_body
+		quote.save()
+		messages.info(request, 'You have successfully edited this quote.')
+	template = 'main/edit_quote.html'
+	args['quote'] = quote
+	return render(request, template, args)
+
